e85285d447bbbecd46ecb46a7e340c8c138ce8d7
Aleksey Lisikh <lizard5472@gmail.com>
2016-07-08 22:14:19 +0300

    Moved from ECMAScript 6 to 5

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const restify = require('restify');
-const skype = require('skype-sdk');
+var fs = require('fs');
+var restify = require('restify');
+var skype = require('skype-sdk');
 
-const botService = new skype.BotService({
+var botService = new skype.BotService({
     messaging: {
         botId: 'dd76f065-6693-471a-a996-cd74cb71c207',
         serverUrl : "https://apis.skype.com",
@@ -12,18 +12,19 @@ const botService = new skype.BotService({
     }
 });
 
-botService.on('contactAdded', (bot, data) => {
-    bot.reply(`Hello ${data.fromDisplayName}! I'm FlowFact Skype bot :)`, true);
+botService.on('contactAdded', function(bot, data) {
+    bot.reply('Hello ' + data.fromDisplayName + '! I\'m FlowFact Skype bot :)', true);
 });
 
-botService.on('personalMessage', (bot, data) => {
-    bot.reply(`Hey ${data.from}. Thank you for your message: "${data.content}".`, true);
+botService.on('personalMessage', function(bot, data) {
+    bot.reply('Hey ' + data.from + '. Thank you for your message: "' + data.content + '".', true);
 });
 
-const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-// const ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-const server = restify.createServer();
+var server = restify.createServer();
 server.post('/v1/chat', skype.messagingHandler(botService));
-server.listen(port);
-console.log('Listening for incoming requests on port ' + port);
+server.listen(port, ipAddress, function() {
+	console.log('%s, listening for incoming requests on port %s', server.name, server.url);
+});
