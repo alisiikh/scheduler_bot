9d6116f479a1279742322874d930425800f9a691
Nata Pana <sancta2012@gmail.com>
2016-07-11 22:43:53 +0300

    Changed webhook for adding contact

--- app.js
+++ app.js
@@ -17,20 +17,44 @@ var botService = new skype.BotService({
 });
 
 botService.on('contactAdded', function(bot, data) {
-    var skypeAddress = new SkypeAddress({ 
-        "skypeId": data.from, 
-        "displayName": data.fromDisplayName,
-        "dateCreated": new Date()
-    });
-    skypeAddress.save(function(err) {
-        if (!err) {
-           console.log("Stored new skype contact with a name: " + data.from);
-        }
-    });
+    var skypeId = data.from;
+    var displayName = data.displayName;
 
-    bot.reply("Hello, " + data.fromDisplayName + "! I've stored you to my " 
-        + "database so that you will not miss my notifications. \n" 
-        + "Data received: \n" + JSON.stringify(data));
+    SkypeAddress.findBySkypeId({ "skypeId": data.from }, function(err, skypeAddresses) {
+        if (err) {
+            console.error("Failed to execute findBySkypeId()");
+            return;
+        }
+
+        if (skypeAddresses.length != 0) {
+            bot.reply("I already have contact with name " + skypeId 
+                + ", so I will remind you when time comes"); 
+
+            bot.reply("Reply true", true);
+            return;
+        }
+
+        var skypeAddress = new SkypeAddress({ 
+            "skypeId": skypeId, 
+            "displayName": displayName,
+            "dateCreated": new Date()
+        });
+
+        skypeAddress.save(function(err) {
+            if (!err) {
+                console.log("Stored new skype contact with a name: " + skypeId);
+            }
+        });
+
+        bot.reply("Hello, " + displayName + "! I've stored you to my " 
+            + "database so that you will not miss my notifications. \n" 
+            + "Data received: \n" + JSON.stringify(data));
+
+        agenda.on('ready', function() {
+            agenda.schedule('in 1 minute', 'notify skype contact', { "skypeId": skypeId, "bot": bot });
+            agenda.start();
+        });
+    });
 });
 
 botService.on('personalMessage', function(bot, data) {
@@ -38,8 +62,8 @@ botService.on('personalMessage', function(bot, data) {
 
     agenda.on('ready', function() {
         agenda.schedule('in 1 minute', 'notify skype contact', { "skypeId": data.from, "bot": bot });
+        agenda.start();
     });
-    agenda.start();
 });
 
 server.post('/v1/chat', skype.messagingHandler(botService));
