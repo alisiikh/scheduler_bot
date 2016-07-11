24bae858d30ce41a301fb290a9c8dec563513c93
Nata Pana <sancta2012@gmail.com>
2016-07-11 21:57:56 +0300

    Fixed mongo config

--- db.js
+++ db.js
@@ -2,4 +2,5 @@ var dbName = 'flowfact-skype-bot';
 var mongoHost = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 var mongoPort = '27017';
 
-module.exports.mongoUrl = 'mongodb://' + mongoHost + ':' + mongoPort + '/' + dbName;
+module.exports.mongoUrl = process.env.MONGODB_URL 
+	|| ('mongodb://' + mongoHost + ':' + mongoPort + '/' + dbName);
