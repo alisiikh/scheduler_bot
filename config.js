4850720b541ae7c93cf011b69c27e28d10bb054c
Aleksey Lisikh <lizard5472@gmail.com>
2016-07-13 23:46:41 +0300

    Changed localhost to 127.0.0.1 in db url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
 	port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	ipAddress: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-	databaseURL: process.env.MONGODB_URL || 'mongodb://localhost:27017/flowfact-skype-bot',
+	databaseURL: process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/flowfact-skype-bot',
 	botId: 'dd76f065-6693-471a-a996-cd74cb71c207',
 	skypeApiURL: "https://apis.skype.com",
 	skypeAppId: process.env.SKYPE_APP_ID,
