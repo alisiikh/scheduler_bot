d390a2757f475b9804f6848af84b15cf4d6cceb2
Aleksey Lisikh <lizard5472@gmail.com>
2016-07-13 09:11:49 +0300

    Removed app_id and secret from js file

--- skype-bot-service.js
+++ skype-bot-service.js
@@ -7,8 +7,8 @@ var botService = new skype.BotService({
         botId: 'dd76f065-6693-471a-a996-cd74cb71c207',
         serverUrl : "https://apis.skype.com",
         requestTimeout : 15000,
-        appId: process.env.SKYPE_APP_ID || "dd76f065-6693-471a-a996-cd74cb71c207",
-        appSecret: process.env.SKYPE_APP_SECRET || "ATxiZN1nDYkdWzpQAO9wbxW"
+        appId: process.env.SKYPE_APP_ID,
+        appSecret: process.env.SKYPE_APP_SECRET
     }
 });
 
