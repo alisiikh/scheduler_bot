c3087b6c6f5131443735edcf6da5515f754960ed
Aleksey Lisikh <lizard5472@gmail.com>
2016-08-10 23:09:19 +0300

    Fixes for MDUtil

--- src/mdutil.js
+++ src/mdutil.js
@@ -2,9 +2,9 @@
 class MDUtil {
     static adaptToMarkdown(content) {
         if (content.indexOf("\r\n") != -1) {
-            return content.replace("\r\n", "\n\n");
-        } else if (content.indexOf("\r\n") != - 1) {
-            return content.replace("\n", "\n\n");
+            return content.replace("\r\n", "\r\n\r\n");
+        } else {
+            return content;
         }
     }
 }
