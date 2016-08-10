cea2d355c0f7d64184fd581ef4f692266228d1d7
Aleksey Lisikh <lizard5472@gmail.com>
2016-08-10 23:15:15 +0300

    Added replaceAll method for String for replacing globally via RegExp

--- src/mdutil.js
+++ src/mdutil.js
@@ -1,8 +1,12 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 
 class MDUtil {
     static adaptToMarkdown(content) {
         if (content.indexOf("\r\n") != -1) {
-            return content.replace("\r\n", "\r\n\r\n");
+            return content.replaceAll("\r\n", "\r\n\r\n");
         } else {
             return content;
         }
